ab076380aeaa20dfbf93764b14ed06edf8b3a329
frederico lopes <fredericolopes@fredericos-MacBook-Pro.local>
2023-06-12 01:42:44 -0300

    nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { HeaderComponent } from "./components/template/header/header.component";
 
+import { MatListModule } from "@angular/material/list";
 import { MatToolbarModule } from "@angular/material/toolbar";
-import { FooterComponent } from './components/template/footer/footer.component';
+import { MatSidenavModule } from "@angular/material/sidenav";
+
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+
+import { NavComponent } from "./components/template/nav/nav.component";
+import { HeaderComponent } from "./components/template/header/header.component";
+import { FooterComponent } from "./components/template/footer/footer.component";
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent],
   imports: [
     BrowserModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
