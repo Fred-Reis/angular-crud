16ed878b34e101bf37c40b2118977a2ccb2c5424
frederico lopes <fredericolopes@fredericos-MacBook-Pro.local>
2023-06-14 22:50:44 -0300

    home view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
+import { MatCardModule } from "@angular/material/card";
 import { MatListModule } from "@angular/material/list";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatSidenavModule } from "@angular/material/sidenav";
@@ -9,13 +10,24 @@ import { MatSidenavModule } from "@angular/material/sidenav";
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
 
+import { HomeComponent } from "./views/home/home.component";
+
 import { NavComponent } from "./components/template/nav/nav.component";
 import { HeaderComponent } from "./components/template/header/header.component";
 import { FooterComponent } from "./components/template/footer/footer.component";
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+    ProductCrudComponent,
+  ],
   imports: [
     BrowserModule,
+    MatCardModule,
     MatListModule,
     MatSidenavModule,
     MatToolbarModule,
